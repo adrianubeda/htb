993420c4566b64b0ab6c4a559630d6bc1c26164e
adrianubedasegu <adrianubedasegu@gmail.com>
2023-09-29 12:22:56 -0400

    Script-P4

--- htbmachines.sh
+++ htbmachines.sh
@@ -27,6 +27,7 @@ function helpPanel(){
 	echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por un nombre de máquina${endColour}"
 	echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por dirección IP${endColour}"
 	echo -e "\t${purpleColour}d)${endColour}${grayColour} Buscar según la dificultad de la máquina${endColour}"
+	echo -e "\t${purpleColour}o)${endColour}${grayColour} Buscar por el sistema operativo${endColour}"
 	echo -e "\t${purpleColour}y)${endColour}${grayColour} Obtener link de la resolución de la máquina${endColour}"
 	echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar panel de ayuda${endColour}"	
 }
@@ -127,27 +128,64 @@ function getYoutubeLink(){
 function searchDificulty() {
 	dificulty="$1"
 	
-	results_check="$(cat bundle.js | grep "dificultad: \"$dificulty\"" -B 5 | grep name | awk 'NF{print $NF}' | tr -d '",' | column)"
+	results_check="$(cat bundle.js | grep "dificultad: \"$dificulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '",' | column)"
 
 	if [ "$results_check" ]; then
-		echo -e "\n ${yellowColour}[+]${endColour}${grayColour} Estas son las máquinas con la dificultad:${endColour}${purpleColour} $dificulty${endColour}\n\n$results_check"
+		echo -e "\n ${yellowColour}[+]${endColour}${grayColour} Estas son las máquinas con la dificultad${endColour}${purpleColour} $dificulty${grayColour}:${endColour}${endColour}\n\n$results_check"
 	else
 		echo -e "\n${redColour}[!] La dificultad proporcionada no existe${endColour}\n"
 	fi
 }
 
+
+function getsOperative(){
+
+	sOperative="$1"
+	
+	sOperative_check="$(cat bundle.js | grep "so: \"$sOperative\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '",' | column)"
+
+	if [ "$sOperative_check" ]; then
+		echo -e "\n ${yellowColour}[+]${endColour}${grayColour} Estas son las máquinas con el SO${endColour}${purpleColour} $sOperative${endColour}${grayColour}:${endColour}\n\n$sOperative_check"
+	else
+		echo -e "\n${redColour}[!] El Sistema Operativo proporcionado no existe${endColour}\n"
+	fi
+
+
+}
+
+
+function getOSDifficultyMachines() {
+	
+	dificulty="$1"
+	sOperative="$2"
+
+	result_checker="$(cat bundle.js | grep "so: \"$sOperative\"" -C 4 | grep "dificultad: \"$dificulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '",' | column)"
+
+	if [ "$result_checker" ]; then		
+		echo -e "\n ${yellowColour}[+]${endColour}${grayColour} Estas son las máquinas con la dificultad${endColour}${purpleColour} $dificulty${endColour}${grayColour} y el SO ${blueColour}$sOperative${endColour}:${endColour}\n\n$result_checker"
+	else
+		echo -e "\n${redColour}[!] La dificultad o el SO proporcionado no existen${endColour}\n"
+	fi
+
+}
+
 # Indicadores
 declare -i parameter_counter=0 # Creamos una variable de tipo entero con -i
 
+# Chivatos
+declare -i chivato_difficulty=0
+declare -i chivato_sOperative=0
+
 # Creamos dos parametros para cuando lancemos el script, si queremos que el parametro tenga un argumento deberemos de ponerle : seguidos, es decir, si queremos que sea
 # así ./htbmachine.sh -m "Nombre_maquina", deberemos indicarle "m:"
-while getopts "m:ui:y:d:h" arg; do # Primero se pone los paremtros que necesitan argumentos, y seguido de los : los que no lo necesitan, en este caso u y h no lo necesitan
+while getopts "m:ui:y:d:o:h" arg; do # Primero se pone los paremtros que necesitan argumentos, y seguido de los : los que no lo necesitan, en este caso u y h no lo necesitan
 	case $arg in
 	 m) machineName="$OPTARG";  let parameter_counter+=1;;
 	 u) let parameter_counter+=2;;
 	 i) ipAddress="$OPTARG"; let parameter_counter+=3;;
 	 y) machineName="$OPTARG"; let parameter_counter+=4;;
-	 d) dificulty="$OPTARG"; let parameter_counter+=5;;
+	 d) dificulty="$OPTARG";  chivato_difficulty=1; let parameter_counter+=5;;
+	 o) sOperative="$OPTARG"; chivato_sOperative=1; let parameter_counter+=6;;
 	 h) ;; # Cuando hagamos -h, llamaremos a la función helpPanel
 	esac
 done
@@ -162,6 +200,10 @@ elif [ $parameter_counter -eq 4 ]; then
 	getYoutubeLink $machineName
 elif [ $parameter_counter -eq 5 ]; then
 	searchDificulty $dificulty
+elif [ $parameter_counter -eq 6 ]; then
+	getsOperative $sOperative
+elif [ $chivato_difficulty -eq 1 ] && [ $chivato_sOperative -eq 1 ]; then
+	getOSDifficultyMachines $dificulty $sOperative
 else
 	helpPanel
 fi
